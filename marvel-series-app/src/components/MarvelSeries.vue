94c0a0ac9aa37b6b81afa2b70ab36b385c81d702
Jacinto Palma Zambrano <39675531+Ranjokan@users.noreply.github.com>
2024-03-12 15:02:18 -0500

    feat: Images
    + Display images with jpg format

--- marvel-series-app/src/components/MarvelSeries.vue
+++ marvel-series-app/src/components/MarvelSeries.vue
@@ -1,9 +1,25 @@
 <template>
     <div>
         <h3>THIS IS MARVELSERIES.VUE</h3>
+        <div class="series-card">
+            <img :src="thumbnail" :alt="title" class="series-image" />
+            <div class="series-details">
+                <h2 class="series-title">{{ title }}</h2>
+                <p class="series-year">Year: {{ year }}</p>
+                <p class="series-types">Types: {{ types }}</p>
+                <div class="related-resources">
+                    <h3>Related Resources</h3>
+                    <ul>
+                        <li v-for="(resource, index) in relatedResources" :key="index">
+                            <a :href="resource.url" target="_blank">{{ resource.name }}</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
         <ul>
-            <li v-for="serie in series">               
-                <router-link :to="{name: 'serie' , params: {id: serie.id}}">{{ serie.title }}</router-link>   
+            <li v-for="serie in series">
+                <router-link :to="{ name: 'serie', params: { id: serie.id } }">{{ serie.title }}</router-link>
             </li>
         </ul>
     </div>
@@ -15,24 +31,24 @@ import axios from 'axios';
 
 export default {
     name: 'MarvelSeries',
-    data(){
-        return{
+    data() {
+        return {
             series: []
         }
     },
-    mounted(){
+    mounted() {
         this.getSeries();
     },
     methods: {
         getSeries: function () {
             axios.get(`https://gateway.marvel.com/v1/public/series?apikey=${public_key}`)
-                .then((result) =>{                  
+                .then((result) => {
                     result.data.data.results.forEach((element) => {
                         this.series.push(element)
                         console.log(element)
                     });
                 })
-                .catch((error)=> {
+                .catch((error) => {
                     console.log(error)
                 })
         }
@@ -41,4 +57,48 @@ export default {
 }
 </script>
 
-<style lang="css"></style>
+<style lang="css">
+.series-card {
+  border: 1px solid #ccc;
+  border-radius: 5px;
+  padding: 20px;
+  margin: 10px;
+  display: flex;
+}
+
+.series-image {
+  width: 150px;
+  height: auto;
+  margin-right: 20px;
+}
+
+.series-details {
+  flex-grow: 1;
+}
+
+.series-title {
+  font-size: 1.2rem;
+  margin-bottom: 10px;
+}
+
+.series-year, .series-types {
+  margin-bottom: 5px;
+}
+
+.related-resources {
+  margin-top: 10px;
+}
+
+.related-resources ul {
+  padding-left: 20px;
+}
+
+.related-resources ul li {
+  margin-bottom: 5px;
+}
+
+.related-resources a {
+  color: #007bff;
+  text-decoration: none;
+}
+</style>
--- marvel-series-app/src/components/Serie.vue
+++ marvel-series-app/src/components/Serie.vue
@@ -2,12 +2,44 @@
     <div>
         <h3>this is character.vue</h3>
         <p>{{ this.$route.params.id }}</p>
+
+        
+        <img :src="url">
     </div>
 </template>
 
 <script>
+import { public_key, secret_key } from '../marvel';
+import axios from 'axios';
+
 export default {
-    name: 'Serie'
+    name: 'Serie',
+    data(){
+        return{
+            serieInfo: [],
+            url: '',
+            size: 'standard_fantastic.jpg'
+        }
+    },
+    mounted(){
+        this.getSerieById();
+    },
+    methods: {
+        getSerieById: function () {
+            var serieId = this.$route.params.id;
+            axios.get(`https://gateway.marvel.com/v1/public/series/${serieId}?apikey=${public_key}`)
+                .then((result) =>{                  
+                    result.data.data.results.forEach((element) => {
+                        this.serieInfo.push(element);
+                        this.url = `${element.thumbnail.path}/${this.size}`;
+                        console.log(element)
+                    });
+                })
+                .catch((error)=> {
+                    console.log(error)
+                })
+        }
+    }
 }
 </script>
 
