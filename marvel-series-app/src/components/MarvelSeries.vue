4c6c62937689e5fc322c7f312b5d59f34fc89a9d
Jacinto Palma Zambrano <39675531+Ranjokan@users.noreply.github.com>
2024-03-13 14:03:20 -0500

    feat: Icons and Links
    + Display icons for each type
    + Display button to redirect to details

--- marvel-series-app/src/components/MarvelSeries.vue
+++ marvel-series-app/src/components/MarvelSeries.vue
@@ -1,18 +1,31 @@
 <template>
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link
+    href="https://fonts.googleapis.com/css2?family=Comic+Neue:ital,wght@0,300;0,400;0,700;1,300;1,400;1,700&display=swap"
+    rel="stylesheet">
   <div>
-    <h3>THIS IS MARVELSERIES.VUE</h3>
+    
     <div class="row" v-for="(row, index) in rows" :key="index">
       <div class="column" v-for="(serie, serieIndex) in row" :key="serieIndex">
         <div class="series-card">
-          <router-link :to="{ name: 'serie', params: { id: serie.id } }">{{ serie.title }}</router-link>
-          <div>
-           <img :src="`${serie.thumbnail.path}/${thumbnailSizeM}`" />
-            
+          <router-link :to="{ name: 'serie', params: { id: serie.id } }" class="series-title">{{ serie.title
+            }}</router-link>
+          <div class="series-image">
+            <img :src="`${serie.thumbnail.path}/${thumbnailSizeM}`" />
+          </div>
+          <div class="series-font">
+            <p>Start Year:{{ serie.startYear }} - End Year:{{ serie.endYear }}</p>
           </div>
           <div>
-            <p>Start Year:{{ serie.startYear }} - End Year:{{ serie.endYear }}</p>
-            <p>Type: {{ serie.type }}</p>
-            <p>{{ serie.description || 'No description available' }}</p>
+            <img v-if="serie.type == 'collection'" src="../assets/icon-collection.png">
+            <img v-else-if="serie.type == 'limited'" src="../assets/icon-limited.png">
+            <img v-else-if="serie.type == 'one shot'" src="../assets/icon-one-shot.png">
+            <img v-else-if="serie.type == 'ongoing'" src="../assets/icon-ongoing.png">
+            <img v-else src="../assets/icon-unknown.png">
+          </div>
+          <div>
+            <router-link :to="{ name: 'serie', params: { id: serie.id } }"><button class="details-button">Details</button></router-link> 
           </div>
         </div>
       </div>
@@ -30,9 +43,12 @@ import { defineComponent, computed } from 'vue';
 
 export default {
   name: 'MarvelSeries',
-  
+  data() {
+    return {
+      isExpanded: false
+    };
+  },
   mounted() {
-   
 
   },
   setup() {
@@ -56,7 +72,12 @@ export default {
 
   },
   methods: {
-    
+    toggleExpand() {
+      this.isExpanded = true;
+    },
+    readMore(selectedSerie) {
+
+    }
   }
 
 }
@@ -73,32 +94,46 @@ export default {
   padding: 0 10px;
 }
 
+.series-font{
+  font-family: "Comic Neue", cursive;
+  font-weight: 700;
+  font-style: normal;
+  color: #f8b007;
+}
+
 .series-card {
-  border: 1px solid #ccc;
-  border-radius: 5px;
+  border: 3px solid #f8b007;
+  border-radius: 8px;
+  background-color: #02030C;
   padding: 20px;
   margin: 10px;
+  color: #FCD883;
+  box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+  display: flex;
+  position: relative;
+  flex: 1;
+  flex-direction: column;
 }
 
 .series-image {
-  width: 150px;
-  height: auto;
-  margin-right: 20px;
+  padding-top: 20px;
 }
 
 .series-details {
   flex-grow: 1;
+  overflow: hidden;
+  text-overflow: ellipsis;
 }
 
 .series-title {
   font-size: 1.2rem;
-  margin-bottom: 10px;
+  margin-bottom: 20px;
+  font-family: "Comic Neue", cursive;
+  font-weight: 700;
+  font-style: normal;
+  color: #f8b007;
 }
 
-.series-year,
-.series-types {
-  margin-bottom: 5px;
-}
 
 .related-resources {
   margin-top: 10px;
@@ -115,5 +150,39 @@ export default {
 .related-resources a {
   color: #007bff;
   text-decoration: none;
+}
+
+.description {
+  overflow: hidden;
+  text-overflow: clip;
+  white-space: break-spaces;
+  max-height: 3em;
+  font-size: small;
+}
+
+.expanded {
+  max-height: initial;
+  white-space: normal;
+}
+
+.details-button {
+  bottom: 10px;
+  right: 10px;
+  background-color: #02030C; 
+  color: #ffd900; 
+  border: 3px solid #ffd900;
+  padding: 8px 16px;
+  border-radius: 20px; 
+  cursor: pointer;
+  transition: opacity 0.3s ease-in-out;
+  margin-top: 20px;
+  font-family: 'Comic Neue';
+  font-weight: 900;
+}
+
+.details-button:hover {
+  background-color: #f75a0a;
+  color: #02030C; 
+  border: 3px solid #02030C;
 }
 </style>
